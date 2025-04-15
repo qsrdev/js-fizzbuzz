8c3568be7bba156022c8b358b8dd528d5c169bcb
qsrdev <matteo.albertini747@gmail.com>
2025-04-15 15:36:17 +0200

    feat: task done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+let fizznumber = 3;
+let buzznumber = 5;
+
 for (let i = 1; i <= 100; i++)
-  if (i % 3 === 0 && i % 5 === 0) {
+  if (i % fizznumber === 0 && i % buzznumber === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+  } else if (i % fizznumber === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0) {
+  } else if (i % buzznumber === 0) {
     console.log("Buzz");
   } else {
     console.log(i);
